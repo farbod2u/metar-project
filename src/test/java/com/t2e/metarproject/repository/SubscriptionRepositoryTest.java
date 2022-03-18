064508a888eef501e9c5d13ca28d00fa1221e2f4
farbod2u <saeedsaf@gmail.com>
2022-03-18 21:23:00 +0330

    subscription unit test

---
+++ src/test/java/com/t2e/metarproject/repository/SubscriptionRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.t2e.metarproject.repository;
+
+import com.t2e.metarproject.entity.Subscription;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class SubscriptionRepositoryTest {
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    @AfterEach
+    void afterEach(){
+        subscriptionRepository.deleteAll();
+    }
+
+    @Test
+    void getByIcaoCode_found() {
+        //given
+        String icaoCode = "LZDA";
+        Subscription entity = new Subscription(icaoCode, null);
+        subscriptionRepository.save(entity);
+
+        //when
+        Optional<Subscription> result = subscriptionRepository.getByIcaoCode(icaoCode);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+        assertThat(result.get().getIcaoCode()).isEqualTo(icaoCode);
+    }
+
+    @Test
+    void getByIcaoCode_notfound() {
+        //given
+        String icaoCode = "LZDA";
+
+        //when
+        Optional<Subscription> result = subscriptionRepository.getByIcaoCode(icaoCode);
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+    }
+}
